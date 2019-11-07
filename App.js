b1530c3c779a21aca2b38fdd6b8a2e4e73705fb5
Charlie <achanont@gmail.com>
2019-11-06 23:52:16 -0600

    first commit

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-window.navigator.userAgent = 'react-native';
+// window.navigator.userAgent = 'react-native';
 import io from 'socket.io-client/dist/socket.io';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+]);
 
 export default class App extends React.Component {
+  state = { name: 'Bob', time: new Date().toLocaleString() };
+
   constructor() {
     super();
-    this.state = { name: 'Bob' };
 
-    this.socket = io('localhost:3000', { jsonp: false });
+    this.socket = io('http://localhost:3000/', { jsonp: false });
 
-    this.socket.on('update', () => this.setState({ name: 'Nate' }));
+    this.socket.on('hiupdate', () => {
+      this.setState({ name: 'Nate' });
+    });
+
+    this.socket.on('hiupdate', () => {
+      const time = new Date().toLocaleString();
+      this.setState({ time: time });
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>{this.state.name}</Text>
+        <Text>{this.state.time}</Text>
       </View>
     );
   }
--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+// var io = require('socket.io')(server);
+const socket = require('socket.io');
+const serverSocket = socket(server);
 
 server.listen(3000);
 // WARNING: app.listen(80) will NOT work here!
@@ -11,10 +13,11 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
+serverSocket.on('connection', socket => {
   console.log(socket.id);
+
   socket.on('update', () => {
     console.log('update in server');
-    io.emit('update');
+    serverSocket.emit('hiupdate');
   });
 });
